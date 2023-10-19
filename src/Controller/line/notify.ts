4b8a9a54f8cc26927682dcfe79d3495245476573
LiuAnBoy <eric141886@gmail.com>
2023-10-20 05:20:09 +0800

    version: 1.0.2

--- src/Controller/line/notify.ts
+++ src/Controller/line/notify.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import queryString from 'query-string';
 import axios from 'axios';
+import * as URL from 'url';
 
 import User from '../../Models/user';
 import Locals from '../../Provider/Locals';
@@ -9,6 +10,12 @@ class NotifyController {
   public static async CombineNotify(req: Request, res: Response) {
     const { id, code, state } = req.query;
 
+    const redirect_uri = URL.format({
+      protocol: process.env.NODE_ENV === 'production' ? 'https' : 'http',
+      host: req.headers.host,
+      pathname: '/line/notify/token',
+    });
+
     try {
       if (!code) {
         const url = queryString.stringifyUrl({
@@ -16,7 +23,7 @@ class NotifyController {
           query: {
             response_type: 'code',
             client_id: Locals.config().notifyChannelID,
-            redirect_uri: `${Locals.config().url}/line/notify/token`,
+            redirect_uri,
             scope: 'notify',
             state: id as string,
           },
@@ -36,7 +43,7 @@ class NotifyController {
       const data = {
         code,
         grant_type: 'authorization_code',
-        redirect_uri: `${Locals.config().url}/line/notify/token`,
+        redirect_uri,
         client_id: Locals.config().notifyChannelID,
         client_secret: Locals.config().notifyChannelSecret,
       };
